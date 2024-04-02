2826d0b5bfa9293a0285f68bbcd66bf72ec9655d
RAP GROUP 7 <eggerval@students.zhaw.ch>
2024-04-02 13:45:33 +0000

    working launch file

--- launch/rap_lawn_mower_launch.py
+++ launch/rap_lawn_mower_launch.py
@@ -4,25 +4,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
+            package='lawn_mower',
+            #namespace='turtlesim1',
+            executable='lawn_mower_node',
+            #name='sim'
         ),
-        '''
-        Node(
-            package='turtlebot3',
-            namespace='turtlebot3',
-            executable='turtlebot3_node',
-            name='lawn_mower'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
-            remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        )'''
     ])
--- lawn_mower/lawn_mower/lawn_mower_node.py
+++ lawn_mower/lawn_mower/lawn_mower_node.py
@@ -1,6 +1,24 @@
-def main():
-    print('Hi from lawn_mower.')
+import rclpy
+from rclpy.node import Node
 
+class LawnMowerNode(Node):
+    def __init__(self) -> None:
+        super().__init__('lawn_mower')
+        self.get_logger().info("Hallo Welt von Gruppe 7")
+
+    def our_algorithm():
+        pass
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    lawnMower_node = LawnMowerNode()
+
+    try:
+        rclpy.spin(lawnMower_node)
+    finally:
+        lawnMower_node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
