cc2bfed1ddf3cfd6e3dc7a1ea18dd04d370733bd
rosinluc <rosinluc@students.zhaw.ch>
2024-04-17 09:24:37 +0000

    working launch file and world is copying correctly after colcon build

--- lawn_mower/setup.py
+++ lawn_mower/setup.py
@@ -12,7 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('world', '*.world*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
